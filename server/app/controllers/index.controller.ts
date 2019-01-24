040cefd2445ff038106bdbcfdcf60e3871d8c4de
Michael Sauget <michaelsauget@gmail.com>
2019-01-23 23:11:02 -0500

    MS A: Montre comment faire pour dire au serveur de checker pour des files dans un path spécifié. Retourne le fichier si trouvé, retourne erreur 400 sinon. On met juste :nomVariable à la fin du path de la route, et on peut ensuite accéder à cette variable par Request.params.<nomVariable>  exemple: req.params.name si on a le path /:name  . IMPORTANT: il faut spécifier le path absolu à partir du root du file system si on a pas défini de path relatif. On a pas besoin de faire appel à un service. On utilise juste la fonction Response.sendFile(<path>) comme montré.

--- server/app/controllers/index.controller.ts
+++ server/app/controllers/index.controller.ts
@@ -24,6 +24,12 @@ export class IndexController {
                 res.json(this.indexService.about());
             });
 
+        router.get("/:name", (req: Request, res: Response, next: NextFunction) => {
+                res.sendFile(
+                    "/Users/michaelsauget/Desktop/Ecole/Poly/5_HIV2019/LOG2900_Projet2/Github/Projet2/server/app/assets/images/"
+                    + req.params.name);
+            });
+
         return router;
     }
 }
